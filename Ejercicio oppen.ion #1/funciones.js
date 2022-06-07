6d56f2762941f1978d6a9fc3c9175e4eebc0aa99
Ksalasceballos <kennethksc2011@gmail.com>
2022-06-07 17:26:14 -0300

    1 file modified

--- Ejercicio oppen.ion #1/funciones.js
+++ Ejercicio oppen.ion #1/funciones.js
@@ -1,59 +1,59 @@
 function init(){
-  //variable
-  var resultado = document.getElementById("resultado");
-  var Reset = document.getElementById("Reset");
-  var Suma = document.getElementById("Suma");
-  var Resta = document.getElementById("Resta");
-  var Multiplicar = document.getElementById("Multiplicar");
-  var Dividiendo = document.getElementById("Dividiendo");
-  var Borrar = document.getElementById("Borrar");
-  var Igual = document.getElementById("Igual");
-  var Uno = document.getElementById("Uno");
-  var Dos= document.getElementById("Dos");
-  var Tres = document.getElementById("Tres");
-  var Cuatro = document.getElementById("Cuatro");
-  var Cinco = document.getElementById("Cinco");
-  var Seis = document.getElementById("Seis");
-  var Siete = document.getElementById("Siete");
-  var Ocho = document.getElementById("Ocho");
-  var Nueve = document.getElementById("Nueve");
-  var Cero = document.getElementById("Cero");
+    //variable
+    var resultado = document.getElementById("resultado");
+    var Reset = document.getElementById("Reset");
+    var Suma = document.getElementById("Suma");
+    var Resta = document.getElementById("Resta");
+    var Multiplicar = document.getElementById("Multiplicar");
+    var Dividiendo = document.getElementById("Dividiendo");
+    var Borrar = document.getElementById("Borrar");
+    var Igual = document.getElementById("Igual");
+    var Uno = document.getElementById("Uno");
+    var Dos= document.getElementById("Dos");
+    var Tres = document.getElementById("Tres");
+    var Cuatro = document.getElementById("Cuatro");
+    var Cinco = document.getElementById("Cinco");
+    var Seis = document.getElementById("Seis");
+    var Siete = document.getElementById("Siete");
+    var Ocho = document.getElementById("Ocho");
+    var Nueve = document.getElementById("Nueve");
+    var Cero = document.getElementById("Cero");
 
-  //Eventos
-  Uno.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "1";
-  }
-  Dos.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "2";
-  }
-  Tres.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "3";
-  }
-  Cuatro.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "4";
-  }
-  Cinco.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "5";
-  }
-  Seis.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "6";
-  }
-  Siete.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "7";
-  }
-  Ocho.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "8";
-  }
-  Nueve.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "9";
-  }
-  Cero.onclick = function(e){
-      resultado.TextContent = resultado.TextContent + "0";
-  }
+    //Eventos
+    Uno.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "1";
+    }
+    Dos.onclick = function(e){
+       resultado.TextContent = resultado.TextContent + "2";
+    }
+    Tres.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "3";
+    }
+    Cuatro.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "4";
+    }
+    Cinco.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "5";
+    }
+    Seis.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "6";
+    }
+    Siete.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "7";
+    }
+    Ocho.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "8";
+    }
+    Nueve.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "9";
+    }
+    Cero.onclick = function(e){
+        resultado.TextContent = resultado.TextContent + "0";
+    }
 
-  Reset.onclick = function(e){
-      resetear();
-  }
+    Reset.onclick = function(e){
+        resetear();
+    }
 
 
 }
