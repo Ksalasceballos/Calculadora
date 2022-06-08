1db67efa855b965ba407565af123a85d567d2048
Ksalasceballos <kennethksc2011@gmail.com>
2022-06-08 00:08:14 -0300

    2 files modified

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
+  <div class="h-screen bg-green-300">
 
-    <h1>Hola mundo</h1>
+    <div class="w-1/3">
 
+      <div class="p-4 bg-white">
+
+        <p class="font-semibold text-gray-700 h-6">1000</p>
+        <p class="font-bold text-3x1 text-gray-900 my-2">10000</p>
+
+      </div>
+
+    </div>
+    
   </div>
 </template>
 
