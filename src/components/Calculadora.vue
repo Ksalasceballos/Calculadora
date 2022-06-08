e15d6ff5ab1ccc464abee3fdb5cd358a14a28bf1
Ksalasceballos <kennethksc2011@gmail.com>
2022-06-08 00:24:57 -0300

    1 file modified

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="h-screen bg-green-300">
-
     <div class="w-1/3">
-
       <div class="p-4 bg-white">
-
         <p class="font-semibold text-gray-700 h-6"></p>
         <p class="font-bold text-3x1 text-gray-900 my-2"></p>
-
       </div>
-
     </div>
-    
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
